b36b16e366ae75cb99cd43826a815269bbfbc7d0
AndrewWebPhp <andreyss100@gmail.com>
2024-01-24 17:53:45 +0200

    24.01 (part 2)

--- app/Http/Actions/Admin/Products/ProductEditAction.php
+++ app/Http/Actions/Admin/Products/ProductEditAction.php
@@ -12,8 +12,6 @@ class ProductEditAction extends BaseAction
 {
     public function __invoke(ProductType $productType, Product $product, ProductEditRequest $request, ProductService $productsService)
     {
-//        dd($request);
-//        dd($request->toDTO());
         $result = $productsService->productEdit($productType, $product, $request->toDTO());
 
         return $this->handleActionResult(route('admin.product.list.page', ['productType' => $productType->id]), $request, $result);
