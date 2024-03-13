d6926ae92fee632e49839d10aec03969616530ab
AndrewWebPhp <andreyss100@gmail.com>
2024-03-13 16:01:52 +0200

    13.03 (part 2)

--- resources/views/pages/store/catalog.blade.php
+++ resources/views/pages/store/catalog.blade.php
@@ -34,8 +34,10 @@
                     <div id="art-products-filter" class="filters">
 
                         <div class="filter-top-wrapper">
-                            <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M1 12L12 1M12 12L1 1" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <svg width="13" height="13" viewBox="0 0 13 13" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path d="M1 12L12 1M12 12L1 1" stroke="black" stroke-width="2" stroke-linecap="round"
+                                      stroke-linejoin="round"/>
                             </svg>
                             <div class="art-filter-heading">
                                 <h4 class="h1 title">{{ trans('base.filter_noun') }}</h4>
@@ -50,7 +52,7 @@
                                     <div id="price-slider" class="price-slider slider-range mb-3">
                                         <div class="currency-wrap">
                                             <div class="input-currency">
-{{--                                                <span class="currency">{{ $baseCurrency->name_short }}</span>--}}
+                                                {{--                                                <span class="currency">{{ $baseCurrency->name_short }}</span>--}}
                                                 <input id="currency-first-main"
                                                        class="currency-first-main sync-input art-form-light-control"
                                                        type="number"
@@ -59,7 +61,7 @@
                                                        name="price_from" placeholder="0">
                                             </div>
                                             <div class="input-currency">
-{{--                                                <span class="currency">{{ $baseCurrency->name_short }}</span>--}}
+                                                {{--                                                <span class="currency">{{ $baseCurrency->name_short }}</span>--}}
                                                 <input id="currency-last-main"
                                                        class="currency-last-main sync-input art-form-light-control"
                                                        type="number"
@@ -70,7 +72,8 @@
                                         </div>
                                         <div class="rangeBar-full"></div>
                                     </div>
-                                    <button type="button" class="btn btn-empty color-dark filter-submit-main">{{ trans('base.apply') }}</button>
+                                    <button type="button"
+                                            class="btn btn-empty color-dark filter-submit-main">{{ trans('base.apply') }}</button>
                                 </div>
                             </div>
 
@@ -85,22 +88,25 @@
                                         <div class="filter-content filter-item"> {{-- filter-item--type-custom--}}
                                             @if($filter->field_type_id === \App\DataClasses\ProductFieldTypeOptionsDataClass::FIELD_TYPE_OPTION)
                                                 @foreach($filter->options as $option)
-                                                    <div class="checkbox" data-toggle="tooltip"> {{-- checkbox-preview--}}
+                                                    <div class="checkbox"
+                                                         data-toggle="tooltip"> {{-- checkbox-preview--}}
 
                                                         {{--custom-checkbox--}}
-                                                        <div class="custom-control position-relative @if(\App\Services\Product\ProductFiltersService::filterOptionChecked($filtersData, $filter->slug, $option->slug)) checked @endif">
+                                                        <div
+                                                            class="custom-control position-relative @if(\App\Services\Product\ProductFiltersService::filterOptionChecked($filtersData, $filter->slug, $option->slug)) checked @endif">
                                                             <input type="radio"
                                                                    id="custom-field-checkbox-{{$filter->id}}-{{$option->id}}-main"
                                                                    name="{{ $filter->slug }}"
                                                                    value="{{ $option->slug }}"
                                                                    @if(\App\Services\Product\ProductFiltersService::filterOptionChecked($filtersData, $filter->slug, $option->slug)) checked @endif>
-                                                            <label class="custom-control-label" for="custom-field-checkbox-{{$filter->id}}-{{$option->id}}-main">{{ $option->name }}</label> {{--custom-control-label--}}
+                                                            <label class="custom-control-label"
+                                                                   for="custom-field-checkbox-{{$filter->id}}-{{$option->id}}-main">{{ $option->name }}</label> {{--custom-control-label--}}
                                                         </div>
                                                     </div>
                                                 @endforeach
 
                                             @elseif($filter->field_type_id === \App\DataClasses\ProductFieldTypeOptionsDataClass::FIELD_TYPE_NUMBER || $filter->field_type_id === \App\DataClasses\ProductFieldTypeOptionsDataClass::FIELD_TYPE_SIZE)
-{{--                                                @dd('oh!')--}}
+                                                {{--                                                @dd('oh!')--}}
                                             @endif
                                         </div>
                                     </div> <!--/filter-box-->
@@ -150,7 +156,8 @@
                                 </div>
                             @endif
 
-                            <div class="toggle-filters-close filter-submit-main btn btn-empty color-dark">{{trans('base.filter')}}</div>
+                            <div
+                                class="toggle-filters-close filter-submit-main btn btn-empty color-dark">{{trans('base.filter')}}</div>
 
                         </form>
 
@@ -163,13 +170,75 @@
                         <h1 class="h2 title">{{ $productType->name }}</h1>
 
                         <div class="art-catalog-top">
+
                             <div id="art-filter-display" class="art-filter-display">
                                 <span>{{ trans('base.filter_noun') }}</span>
-                                <svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M3.87479 8.426V0.973999H4.68479V8.426H3.87479ZM0.454789 5.096V4.322H8.10479V5.096H0.454789Z" fill="black"/>
+                                <svg width="9" height="9" viewBox="0 0 9 9" fill="none"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M3.87479 8.426V0.973999H4.68479V8.426H3.87479ZM0.454789 5.096V4.322H8.10479V5.096H0.454789Z"
+                                        fill="black"/>
                                 </svg>
                             </div>
+
+                            <div class="menu-right-dropdown">
+                                <div class="right-dropdown-row">
+                                    <div>
+                                        <div class="dropdown dropdown-custom mb-1 mb-md-0">
+                                            <button
+                                                class="btn btn-dropdown dropdown-toggle d-block"
+                                                type="button" id="dropdownMenuButton" data-toggle="dropdown"
+                                                aria-haspopup="true" aria-expanded="false">
+                                                <span class="text-left">{{ trans('base.sort_by') }}:</span>
+                                                @isset($filtersData['sort_by'])
+                                                    <span class="text-right">{{ App\DataClasses\ProductSortOptionsDataClass::get()->where('id', $filtersData['sort_by'])->first()['name'] }}</span>
+                                                @else
+                                                    <span class="text-right">{{ App\DataClasses\ProductSortOptionsDataClass::get()->where('is_active_by_default')->first()['name'] }}</span>
+                                                @endisset
+
+                                            </button>
+                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                @foreach(App\DataClasses\ProductSortOptionsDataClass::get() as $sortFilter)
+                                                    <a class="dropdown-item sort-by-option @if(!isset($filtersData['sort_by']) && $sortFilter['is_active_by_default'] || isset($filtersData['sort_by']) && $filtersData['sort_by'] == $sortFilter['id']) active @endif"
+                                                       href="#"
+                                                       id="{{ $sortFilter['id'] }}">{{ $sortFilter['name'] }}</a>
+                                                @endforeach
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div class="dropdown dropdown-custom art-sort-count-wrapper">
+                                            <button
+                                                class="btn btn-dropdown d-block dropdown-toggle"
+                                                type="button" id="dropdownMenuButton2" data-toggle="dropdown"
+                                                aria-haspopup="true" aria-expanded="false">
+                                                <span class="text-left">{{ trans('base.show_items_per_page') }}:</span>
+                                                @isset($filtersData['per_page'])
+                                                    <span class="text-right">{{ $filtersData['per_page'] }}</span>
+                                                @else
+                                                    <span class="text-right">24</span>
+                                                @endisset
+                                            </button>
+                                            <div class="dropdown-menu dropdown-menu-right"
+                                                 aria-labelledby="dropdownMenuButton2">
+                                                <a class="dropdown-item @if(!isset($filtersData['per_page']) || (isset($filtersData['per_page']) && $filtersData['per_page'] == 24)) active @endif"
+                                                   href="#"
+                                                   id="show-24-items-per-page">24 {{ trans('base.per_page') }}</a>
+                                                <a class="dropdown-item @if(isset($filtersData['per_page']) && $filtersData['per_page'] == 36) active @endif"
+                                                   href="#"
+                                                   id="show-36-items-per-page">36 {{ trans('base.per_page') }}</a>
+                                                <a class="dropdown-item @if(isset($filtersData['per_page']) && $filtersData['per_page'] == 48) active @endif"
+                                                   href="#"
+                                                   id="show-48-items-per-page">48 {{ trans('base.per_page') }}</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
+
+
 
                         @if( count($productsPaginated) > 0 )
                             <div class="art-product-list art-three-column">
@@ -182,8 +251,8 @@
                         @endif
                     </div>
 
-                <!--Pagination-->
-                {{ $productsPaginated->links('pagination.store') }}
+                    <!--Pagination-->
+                    {{ $productsPaginated->links('pagination.store') }}
                 </div> <!--/product items-->
 
             </div><!--/row-->
