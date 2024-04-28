80911a3321fc77cbaffec54e8e1d43b80c60459b
AndrewWebPhp <andreyss100@gmail.com>
2024-04-28 21:41:26 +0300

    28.04 (part 14)

--- resources/views/layouts/store-main.blade.php
+++ resources/views/layouts/store-main.blade.php
@@ -83,12 +83,12 @@
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js" defer></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js" defer></script>
     <![endif]-->
 
     @if(Route::currentRouteName() === 'store.home')
-        <script type="application/ld+json">
+        <script type="application/ld+json" defer>
             {
                 "@context": "https://schema.org",
                 "@type": "Organization",
@@ -118,7 +118,7 @@
     @endif
 
     <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    <script defer>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
@@ -129,7 +129,7 @@
 
 <body>
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P9KHGB8T" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P9KHGB8T" height="0" width="0" style="display:none;visibility:hidden" defer></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 
 
