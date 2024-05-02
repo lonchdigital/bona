d2703c13c9ca5e4a70a458eb66379f1623aa7c66
AndrewWebPhp <andreyss100@gmail.com>
2024-05-02 15:20:52 +0300

    2.05 (part 4)

--- resources/views/pages/store/delivery-page.blade.php
+++ resources/views/pages/store/delivery-page.blade.php
@@ -47,7 +47,7 @@
                         <div class="col-md-5 video-side">{!! $deliveryConfig->iframe !!}</div>
                     @else
                         <div class="col-md-5 image-side">
-                            <img src="{{ $deliveryConfig->imageUrl }}" alt="block image" loading="lazy">
+                            <img src="{{ $deliveryConfig->imageUrl }}" alt="block image">
                         </div>
                     @endif
                     <div class="col-md-7 desc-side">
