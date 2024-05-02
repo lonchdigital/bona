98e956fcb4f6a9af6bfb7d1fc64d1cc475c9c257
AndrewWebPhp <andreyss100@gmail.com>
2024-05-02 17:05:20 +0300

    2.05 (part 10)

--- resources/views/pages/store/partials/brand_item.blade.php
+++ resources/views/pages/store/partials/brand_item.blade.php
@@ -1,7 +1,7 @@
 <div class="item">
     <a href="{{ App\Helpers\MultiLangRoute::getMultiLangRoute('store.catalog-by-brand.page', ['brand' => $brand]) }}">
         @if(!is_null($brand->logo_image_path))
-            <img src="{{$brand->logo_image_url}}" alt="Brand logo">
+            <img src="{{$brand->logo_image_url}}" alt="Brand logo" loading="lazy">
         @else
             {{ $brand->name }}
         @endif
