b1448f99eb9bfb3e7dac1f0f96ae05d3ce2979bf
AndrewWebPhp <andreyss100@gmail.com>
2024-05-02 16:01:10 +0300

    2.05 (part 5)

--- resources/views/layouts/store-main.blade.php
+++ resources/views/layouts/store-main.blade.php
@@ -89,7 +89,7 @@
     <![endif]-->
 
     @if(Route::currentRouteName() === 'store.home')
-        <script type="application/ld+json" defer>
+        <script type="application/ld+json">
             {
                 "@context": "https://schema.org",
                 "@type": "Organization",
@@ -119,7 +119,7 @@
     @endif
 
     <!-- Google Tag Manager -->
-    <script defer>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
@@ -130,7 +130,7 @@
 
 <body>
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P9KHGB8T" height="0" width="0" style="display:none;visibility:hidden" defer></iframe></noscript>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P9KHGB8T" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 
 
