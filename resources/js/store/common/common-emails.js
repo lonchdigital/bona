afa873c0104445bb65061d19e4dc072c033154ae
AndrewWebPhp <andreyss100@gmail.com>
2024-05-02 14:09:00 +0300

    2.05 (part 3)

--- resources/js/store/common/common-emails.js
+++ resources/js/store/common/common-emails.js
@@ -159,6 +159,46 @@ export default {
             }
         }
 
+
+
+        // FAQs accordion
+        // ----------------------------------------------------------------
+        var acc = document.getElementsByClassName("accordion");
+        var i;
+
+        for (i = 0; i < acc.length; i++) {
+            acc[i].addEventListener("click", function() {
+
+                if( !$(this).hasClass('active') ) {
+                    $('.accordion').removeClass('active');
+                    $('.art-panel').removeAttr("style");
+                }
+
+                this.classList.toggle("active");
+                var panel = this.nextElementSibling;
+                if (panel.style.maxHeight) {
+                    panel.style.maxHeight = null;
+                } else {
+                    panel.style.maxHeight = (panel.scrollHeight + 100) + "px";
+                }
+            });
+        }
+
+        // Strecher accordion
+        // ----------------------------------------------------------------
+        var $strecherItem = $('.stretcher-item');
+        $strecherItem.bind({
+            mouseenter: function (e) {
+                $(this).addClass('active');
+                $(this).siblings().addClass('inactive');
+            },
+            mouseleave: function (e) {
+                $(this).removeClass('active');
+                $(this).siblings().removeClass('inactive');
+            }
+        });
+
+
     }
 };
 
