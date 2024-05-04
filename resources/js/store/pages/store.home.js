7c7f034b60cd7939cb14c49826ea6c4328346c0f
AndrewWebPhp <andreyss100@gmail.com>
2024-05-04 19:30:01 +0300

    4.05 (part 11)

--- resources/js/store/pages/store.home.js
+++ resources/js/store/pages/store.home.js
@@ -1,5 +1,13 @@
+console.log('are we here??? 2222');
 
+import {swipe} from './store.home/swiperTest';
+swipe();
+
+/*
 export default async function () {
+
+    console.log('are we here??? 11111');
+
     const [
         Swiper,
     ] = await Promise.all([
@@ -8,3 +16,4 @@ export default async function () {
 
     Swiper.init();
 }
+*/
