f57ec418c0b75c00ae24eb0f52edb28ca2d1982d
AndrewWebPhp <andreyss100@gmail.com>
2024-05-04 23:15:42 +0300

    4.05 (part 19)

--- resources/views/layouts/store-main.blade.php
+++ resources/views/layouts/store-main.blade.php
@@ -68,9 +68,9 @@
 
 {{--    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">--}}
 {{--    <link rel="stylesheet" href="{{ asset('assets/css/animate.min.css') }}">--}}
-    <link rel="preload" href="{{ asset('assets/css/font-awesome.min.css') }}" as="style" onload="this.rel='stylesheet'">
-    <link rel="preload" href="{{ asset('assets/css/furniture-icons.min.css') }}" as="style" onload="this.rel='stylesheet'">
-    <link rel="preload" href="{{ asset('assets/css/linear-icons.min.css') }}" as="style" onload="this.rel='stylesheet'">
+    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/furniture-icons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/linear-icons.min.css') }}">
 
 
     {{--    <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.min.css') }}">--}}
