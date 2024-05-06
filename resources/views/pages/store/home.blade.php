42e2693d1b11d6826281ca0c19a874146666fedd
AndrewWebPhp <andreyss100@gmail.com>
2024-05-06 12:34:38 +0300

    6.05 (part 7)

--- resources/views/pages/store/home.blade.php
+++ resources/views/pages/store/home.blade.php
@@ -55,7 +55,8 @@
         </div> <!--/owl-slider-->
 
         <div class="owl-slider art-slider-mobile">
-            <div class="single-slide" style="background-image:url({{ asset('storage/bg-images/home-slider-mob-bg.jpg') }})">
+{{--            <div class="single-slide" style="background-image:url({{ asset('storage/bg-images/home-slider-mob-bg.jpg') }})">--}}
+            <div class="single-slide" style="background-color:#000">
                 <div class="box">
                     <div class="container">
                         <h2 class="title animated h1" data-animation="fadeInDown">{{ $slides[0]->title }}</h2>
