58bcef2bcefe9fa03e3d46da0fc254dfac24e9c6
AndrewWebPhp <andreyss100@gmail.com>
2024-06-07 21:43:16 +0300

    7.06 (part 5)

--- resources/views/pages/auth/confirm-email.blade.php
+++ resources/views/pages/auth/confirm-email.blade.php
@@ -1,17 +1,20 @@
 @extends('layouts.store-main')
 
 @section('content')
+
+    @include('pages.store.partials.page_header', ['links' => ['own' => trans('auth.confirm_your_email')]])
+
     <main class="main pt-5">
         <div class="content">
             <section>
                 <div class="container">
                     <div class="row d-flex justify-content-md-center">
                         <div class="col-lg-6 mb-5 d-flex flex-column align-items-center">
-                        <h2 class="mt-5 text-center">{{ trans('auth.confirm_your_email') }}</h2>
-                        <p class="mt-5 text-center w-75">{{ trans('auth.confirm_your_email_to_finish') }}</p>
-                        <p class="text-center">
-                            <a class="btn btn-outline-black" href="{{ App\Helpers\MultiLangRoute::getMultiLangRoute('auth.sign-in.page') }}">{{ trans('auth.go_to_sign_in') }}</a>
-                        </p>
+                            <h2 class="mt-5 text-center">{{ trans('auth.confirm_your_email') }}</h2>
+                            <p class="mt-5 text-center w-75">{{ trans('auth.confirm_your_email_to_finish') }}</p>
+                            <p class="text-center mt-4">
+                                <a class="btn btn-main" href="{{ App\Helpers\MultiLangRoute::getMultiLangRoute('auth.sign-in.page') }}">{{ trans('auth.go_to_sign_in') }}</a>
+                            </p>
                         </div>
                     </div>
                 </div>
--- resources/views/pages/auth/sign-up.blade.php
+++ resources/views/pages/auth/sign-up.blade.php
@@ -14,10 +14,10 @@
                                 <form action="{{ route('auth.sign-up') }}" method="POST" class="form-content d-flex justify-content-center m-5 flex-column">
                                     @csrf
 
-                                    <div class="w-full d-flex justify-content-lg-around flex-column flex-lg-row">
+                                    <div class="w-full d-flex justify-content-lg-between flex-column flex-lg-row">
                                         <div class="form-group lg-w-45">
                                             <label class="custom-control-label2" for="first_name">{{ trans('auth.first_name') }}</label>
-                                            <input class="form-control" placeholder="{{ trans('auth.first_name_placeholder') }}" type="text" name="first_name" value="{{ old('first_name') }}"/>
+                                            <input class="art-form-light-control" placeholder="{{ trans('auth.first_name_placeholder') }}" type="text" name="first_name" value="{{ old('first_name') }}"/>
                                             @error('first_name')
                                             <div class="text-danger">{{ $message }}</div>
                                             @enderror
@@ -25,17 +25,17 @@
 
                                         <div class="form-group lg-w-45">
                                             <label class="custom-control-label2" for="last_name">{{ trans('auth.last_name') }}</label>
-                                            <input class="form-control" placeholder="{{ trans('auth.last_name_placeholder') }}" type="text" name="last_name" value="{{ old('last_name') }}"/>
+                                            <input class="art-form-light-control" placeholder="{{ trans('auth.last_name_placeholder') }}" type="text" name="last_name" value="{{ old('last_name') }}"/>
                                             @error('last_name')
                                             <div class="text-danger">{{ $message }}</div>
                                             @enderror
                                         </div>
                                     </div>
 
-                                    <div class="w-full d-flex justify-content-lg-around flex-column flex-lg-row">
+                                    <div class="w-full d-flex justify-content-lg-between flex-column flex-lg-row">
                                         <div class="form-group lg-w-45">
                                             <label class="custom-control-label2" for="email">{{ trans('auth.email') }}</label>
-                                            <input id="email" class="form-control" placeholder="{{ trans('auth.email_placeholder') }}" type="text" name="email" value="{{ old('email') }}"/>
+                                            <input id="email" class="art-form-light-control" placeholder="{{ trans('auth.email_placeholder') }}" type="text" name="email" value="{{ old('email') }}"/>
                                             @error('email')
                                             <div class="text-danger">{{ $message }}</div>
                                             @enderror
@@ -43,18 +43,18 @@
 
                                         <div class="form-group lg-w-45">
                                             <label class="custom-control-label2" for="phone">{{ trans('auth.phone') }}</label>
-                                            <input id="phone" class="form-control" placeholder="{{ trans('auth.phone_placeholder') }}" type="text" name="phone" value="{{ old('phone') }}"/>
+                                            <input id="phone" class="art-form-light-control" placeholder="{{ trans('auth.phone_placeholder') }}" type="text" name="phone" value="{{ old('phone') }}"/>
                                             @error('phone')
                                             <div class="text-danger">{{ $message }}</div>
                                             @enderror
                                         </div>
                                     </div>
 
-                                    <div class="w-full d-flex justify-content-lg-around flex-column flex-lg-row">
+                                    <div class="w-full d-flex justify-content-lg-between flex-column flex-lg-row">
                                         <div class="form-group lg-w-45">
                                             <label class="custom-control-label2" for="password">{{ trans('auth.password') }}</label>
                                             <div class="d-flex flex-row align-items-center justify-content-end password-input">
-                                                <input id="password" class="form-control block w-full" type="password" name="password"/>
+                                                <input id="password" class="art-form-light-control block w-full" type="password" name="password"/>
 
                                             </div>
 
@@ -66,7 +66,7 @@
                                         <div class="form-group lg-w-45">
                                             <label class="custom-control-label2" for="password_confirmation">{{ trans('auth.password_confirmation') }}</label>
                                             <div class="d-flex flex-row align-items-center justify-content-end password-input">
-                                                <input id="password_confirmation" class="form-control block w-full" type="password" name="password_confirmation"/>
+                                                <input id="password_confirmation" class="art-form-light-control block w-full" type="password" name="password_confirmation"/>
 
                                             </div>
                                             @error('password_confirmation')
@@ -75,7 +75,7 @@
                                         </div>
                                     </div>
 
-                                    <button class="mt-5 btn btn-outline-black" type="submit">{{ trans('auth.sign_up') }}</button>
+                                    <button class="mt-5 btn btn-main" type="submit">{{ trans('auth.sign_up') }}</button>
                                 </form>
                         </div>
                     </div>
