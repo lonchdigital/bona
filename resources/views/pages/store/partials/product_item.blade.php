d620921224823b15182e8fc3a0876a4a63ee75e3
AndrewWebPhp <andreyss100@gmail.com>
2024-07-24 14:31:47 +0300

    24.07 (part 1)

--- resources/views/pages/store/partials/product_item.blade.php
+++ resources/views/pages/store/partials/product_item.blade.php
@@ -5,17 +5,26 @@
                 <img src="{{ $product->preview_image_url }}" alt="Product Image" loading="lazy">
             </div>
             <div class="text">
-{{--                <div class="product-title">{{ Illuminate\Support\Str::limit($product->name, 55) }}</div>--}}
                 <div class="product-title">{{ $product->name }}</div>
                 <div class="price-wrapper">
-                    @if($product->old_price > $product->price)
-                        <span class="card-link-price--hot">{{ $product->price.' '.$baseCurrency->name_short }}</span>
-                        <span class="card-link-price--old">{{ $product->old_price.' '.$baseCurrency->name_short }}</span>
-                    @else
-                        <span class="price">{{ $product->price }}</span>
-                        <span class="currency">{{ $baseCurrency->name_short }}</span>
-                    @endif
+                    <div class="data-left">
+                        @if($product->old_price > $product->price)
+                            <span class="card-link-price--hot">{{ $product->price.' '.$baseCurrency->name_short }}</span>
+                            <span class="card-link-price--old">{{ $product->old_price.' '.$baseCurrency->name_short }}</span>
+                        @else
+                            <span class="price">{{ $product->price }}</span>
+                            <span class="currency">{{ $baseCurrency->name_short }}</span>
+                        @endif
+                    </div>
+                    <div class="data-right">
+                        <span class="cart-icon-block">
+                            <i class="icon icon-cart"></i>
+                        </span>
+
+                    </div>
+
                 </div>
+                <span>{{ \App\DataClasses\ProductStatusDataClass::get($product->availability_status_id)['name'] }}</span>
             </div>
         </a>
     </div>
