eef95282eef8363101d8329a17478cc31c7a9b14
AndrewWebPhp <andreyss100@gmail.com>
2024-11-20 13:45:45 +0200

    Added monobank part payments

--- app/DataClasses/PaymentTypesDataClass.php
+++ app/DataClasses/PaymentTypesDataClass.php
@@ -7,6 +7,7 @@ class PaymentTypesDataClass implements BaseDataClass
     const CASH_PAYMENT = 1;
     const CARD_PAYMENT = 2;
     const CARD_PAYMENT_PAYPART = 3;
+    const CARD_PAYMENT_PAYPART_MONO_BANK = 4;
 
     public static function get(?int $item = null): mixed
     {
@@ -14,18 +15,22 @@ class PaymentTypesDataClass implements BaseDataClass
             [
                 'id' => self::CASH_PAYMENT,
                 'name' => trans('base.checkout_payment_cash'),
-                'internal_name' => 'cash',
+                'internal_name' => 'cash'
             ],
             [
                 'id' => self::CARD_PAYMENT,
                 'name' => trans('base.checkout_payment_card'),
-                'internal_name' => 'card',
+                'internal_name' => 'card'
             ],
             [
                 'id' => self::CARD_PAYMENT_PAYPART,
                 'name' => trans('base.checkout_payment_paypart'),
-                'internal_name' => 'PP',
-            ],
+                'internal_name' => 'PP'
+            ],[
+                'id' => self::CARD_PAYMENT_PAYPART_MONO_BANK,
+                'name' => trans('base.checkout_payment_paypart_mono_bank'),
+                'internal_name' => 'PP mono'
+            ]
         ]);
 
         if ($item) {
