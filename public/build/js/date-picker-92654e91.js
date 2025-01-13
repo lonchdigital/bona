723c4ed226e792fe6b4690b84de47cc74d9abca6
AndrewWebPhp <andreyss100@gmail.com>
2025-01-13 15:05:51 +0200

    Make loop home slider icons

---
+++ public/build/js/date-picker-92654e91.js
@@ -0,0 +1,1 @@
+import{f as e}from"./index-a4e39586.js";/* empty css                  */document.addEventListener("DOMContentLoaded",function(){e(".datepicker",{enableTime:!0,dateFormat:"Y-m-d H:i:S"})});
