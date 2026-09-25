2b1e6329c4eab8f74e1202046ac186dba2489211
Lonch <info@lonch.digital>
2026-09-25 09:25:03 +0300

    Add MVM A-2005 to A-2010 content

--- tests/Feature/ProductContentImporterTest.php
+++ tests/Feature/ProductContentImporterTest.php
@@ -969,6 +969,11 @@ class ProductContentImporterTest extends TestCase
         $this->assertMvmBatchImports('2026_09_25_mvm_a1209_a2004_batch_01.json', 24);
     }
 
+    public function test_it_imports_the_mvm_a2005_to_a2010_batch_and_keeps_every_variant_unique(): void
+    {
+        $this->assertMvmBatchImports('2026_09_25_mvm_a2005_a2010_batch_01.json', 27);
+    }
+
     private function assertMvmBatchImports(string $filename, int $expectedProducts): void
     {
         $path = database_path("content/products/{$filename}");
